d14f5953a1ed3d37dac717e79ea25df7fe937ce3
aidantheflame <aidantheflame@a73b7efb-06f6-03e8-a959-e71aee5b83a6>
2012-04-17 02:50:18 +0000

    otak-atik model

---
+++ src/Tubes2OOP.java
@@ -0,0 +1,177 @@
+
+package tubes2.oop;
+
+
+import java.util.*;
+import java.io.*;
+import java.lang.reflect.*;
+
+/**
+ * Kelas utama kamus versi 1
+ * @author Jordan, Adriel, and Wilson
+ */
+public class Tubes2OOP {
+
+    /**
+     * Main program
+     * @param args the command line arguments
+	 * @exception IOException
+	 *              mengecek apakah input/output sesuai
+	 * @exception InterruptedException
+	 *              mengecek apakah terjadi interupsi saat pelaksanaan program
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Console console = System.console();
+        String [] bahasa = {"Indonesia","Inggris"};
+        modelmapone model = new modelmapone(2,bahasa);
+        viewmapone view = new viewmapone();
+
+        model.readinput(bahasa[0],bahasa[1],"DicIndonesiaEnglish.txt");
+
+        BufferedReader brin = new BufferedReader(new InputStreamReader(System.in));
+        Scanner in = new Scanner(System.in);
+		
+//		System.out.println(model.getterjemahan("Indonesia","apel").get(0).getarti());
+
+        String bahasadef="Indonesia";
+        String bahasadua="Inggris";
+        int i=7;
+        do
+        {
+                i = view.printmenu(in);
+                switch (i)
+                {
+                        case 1:	{
+                                    view.cetak("Input Kata : ");
+                                    String s1 = view.getline(brin);
+									view.cetak("Terjemahan Kata : ");
+									String s2 = view.getline(brin);
+									view.cetak("Tipe Kata : ");
+									String s3 = view.getline(brin);
+									String [] hasil = model.detectmap(s1);
+                                    int a = 0;
+                                    for (String temp : hasil)
+                                    {
+                                            a++;
+                                    }
+                                    a--;
+                                    if (a!=0)
+                                    {
+                                            if (s2.compareTo(model.getterjemahan(bahasadua,s1).get(0).getarti())==0)
+											{
+												view.cetak("Kata sudah ada dalam database");
+											}
+											else
+											{
+												int j = model.tambahterjemahan(bahasadef,bahasadua,s1,s2,s3);
+												if (j==0)
+												view.cetakln("Entry baru berhasil disimpan.");
+												else
+												view.cetakln("Entry baru gagal disimpan.");     
+											}
+									}
+                                    else
+                                    {
+										int j = model.tambahterjemahan(bahasadef,bahasadua,s1,s2,s3);
+										if (j==0)
+												view.cetakln("Entry baru berhasil disimpan.");
+										else
+												view.cetakln("Entry baru gagal disimpan.");       
+                                    }
+                                    break;
+                                }
+                        case 2:	{
+                                    String temp = bahasadef;
+                                    bahasadef = bahasadua;
+                                    bahasadua = temp;
+                                    view.cetakln("Bahasa " + bahasadua + " -> Bahasa " + bahasadef);
+                                    break;
+                                }			
+                        case 3: {
+                                    view.printpetunjuk();
+                                    view.cetak("Input Kata : ");
+                                    String s = view.getline(brin);
+                                    String hasil = model.getterjemahan(bahasadef,s).get(0).getarti();
+                                    String tipe = model.getterjemahan(bahasadef,s).get(0).gettype();
+                                    if (hasil.compareTo("")!=0)
+                                    {
+                                            view.cetak("Terjemahan Kata : ");
+                                            view.cetakln(hasil);
+                                            view.cetak("Tipe Kata : ");
+                                            view.cetakln(tipe);
+                                    }
+                                    else
+                                    {
+                                            view.cetakln("Kata tidak ada dalam database!");
+                                    }
+                                    break;
+                                }
+                        case 4:	{
+                                    view.cetak("Input Kata : ");
+                                    String s = view.getline(brin);
+
+                                    String [] hasil = model.detectmap(s);
+                                    int j = 0;
+                                    for (String temp : hasil)
+                                    {
+                                            j++;
+                                    }
+                                    j--;
+                                    if (j!=0)
+                                    {
+                                            view.cetak("Kata dalam ");
+                                            for (int k=0;k<j;k++)
+                                            {
+                                                    view.cetak(hasil[k]);
+                                            }
+                                            view.cetakln("");
+                                    }
+                                    else
+                                    {
+                                            view.cetakln("Kata tidak ada dalam database");
+                                    }
+                                    break;
+                                }
+                        case 5: {
+                                    model.cetakmap(bahasadef);
+                                    break;
+                                }
+                        case 6:	{
+                                    try
+                                    {
+                                        Class c = TreeMap.class;
+                                        Method m[] = c.getDeclaredMethods();
+                                        Field f[] = c.getDeclaredFields();
+                                        view.cetakln("Struktur TreeMap");
+                                        view.cetakln("Attribut:");
+                                        for (int j=0;j<f.length;j++)
+                                        {
+                                            view.cetakln(f[j].toString());
+                                        }
+                                        view.cetakln("Method");
+                                        for (int j=0;j<m.length;j++)
+                                        {
+                                            view.cetakln(m[j].toString());
+                                        }
+                                    }
+                                    catch (Throwable e)
+                                    {
+                                        System.err.println(e);
+                                    }
+                                    break;
+                                }
+                        case 7: {
+                                    view.cetakln(" *** Terima Kasih sudah menggunakan program ini ***");
+                                    break;
+                                }
+                        default:{
+                                    view.cetakln("Perintah masukan salah");
+                                    break;
+                                }
+                }
+        }while (i!=7);
+        model.writeoutput(bahasa[0],"DicIndonesiaEnglis.txt");
+        in.close();
+    }
+}
--- src/modelmapone.java
+++ src/modelmapone.java
@@ -1,198 +1,324 @@
-//modelmapone.java
+
+package tubes2.oop;
 
 import java.util.*;
 import java.io.*;
-import java.io.Console;
+import java.util.Vector;
 
-class modelmapone
-{
-	private SortedMap map[];
-	private int jumlah;
-	private SortedMap bahasa = new TreeMap();
-	private String [] namabahasa;
-	private SortedMap tipe = new TreeMap();
-	
-	public modelmapone (int Jumlah, String [] Bahasa)
-	{
-		jumlah=Jumlah;
-		map = new SortedMap[jumlah];
-		for (int i=0;i<jumlah;++i)
-		{
-			map[i] = new TreeMap();
-		}
-		int i = 0;
-		for (String s : Bahasa)
-		{
-			bahasa.put(s,i);
-			i++;
-		}
-		namabahasa = new String[i];
-		i = 0;
-		for (String s : Bahasa)
-		{
-			namabahasa[i]=s;
-			i++;
-		}
-	}
-	
-	public void cetakmap (int i)
-	{
-		viewmapone view = new viewmapone();
-		Iterator iterator = map[i].keySet().iterator();
-		while (iterator.hasNext()) 
-		{
-			Object key = iterator.next();
-			view.cetakln(key + " : " + map[i].get(key) + " (" + tipe.get(key) + ")");
-		}
-	}
-	
-	public String [] detectmap (String s)
-	{
-		String [] value = new String[jumlah];
-		int j=0;
-		for (int i=0;i<jumlah;++i)
-		{
-			if (map[i].get(s)!=null)
-			{
-				value[j]=namabahasa[i];
-				j++;
-			}
-		}
-		return value;
-	}
-	
-	public String getterjemahan (int Bahasa, String s)
-	{
-		if (map[Bahasa].get(s)!=null)
-			return (String) map[Bahasa].get(s);
-		return "";
-	}
-	
-	public String gettipe (String s)
-	{
-		return (String) tipe.get(s);
-	}
-	
-	public SortedMap getmap (int i)
-	{
-		return map[i];
-	}
-	
-	public int tambahterjemahan (int Bahasa1, int Bahasa2, String s1, String s2, String s3)
-	{
-		boolean cek = false;
-		if (map[Bahasa1].get(s1)==null)
-		{
-			map[Bahasa1].put(s1,s2);
-			tipe.put(s1,s3);
-			cek = true;
-		}
-		if (map[Bahasa2].get(s2)==null)
-		{
-			map[Bahasa2].put(s2,s1);
-			tipe.put(s2,s3);
-			cek = true;
-		}
-		if (cek)
-			return 0;
-		return -1;
-	}
-	
-	public void setmap (SortedMap m, int i)
-	{
-		Iterator iterator = m.keySet().iterator();
-		while (iterator.hasNext()) 
-		{
-			Object key = iterator.next();
-			if (map[i].get(key)==null)
-				map[i].put(key,m.get(key));
-		}
-	}
-	
-	public void readinput (String bahasa1, String bahasa2, String namafile)
-	{
-		try
-		{
-			int bhs1 = (int)bahasa.get(bahasa1);
-			int bhs2 = (int)bahasa.get(bahasa2);
-			
-			FileInputStream fstream = new FileInputStream(namafile);
-			DataInputStream fin = new DataInputStream(fstream);
-			BufferedReader brf = new BufferedReader(new InputStreamReader(fin));
-			
-			String s1="";
-			String s2="";
-			String s3="";
-			boolean cek = false;
-			boolean cekspecial = false;
-			int tempint;
-			while ((tempint=brf.read())!=-1)
-			{
-				String temp = ""+(char)tempint;
-				if (temp.compareTo(")")==0)
-					cekspecial = false;
-				else if (cekspecial)
-					s3 = s3+temp;
-				else if (temp.compareTo("(")==0)
-					cekspecial = true;
-				else if (temp.compareTo(":")==0)
-					cek = true;
-				else if (temp.compareTo(".")==0)
-				{
-					cek = false;
-					if (map[bhs1].get(s1)==null)
-					{
-						map[bhs1].put(s1,s2);
-						tipe.put(s1,s3);
-					}
-					if (map[bhs2].get(s2)==null)
-					{
-						map[bhs2].put(s2,s1);
-						tipe.put(s2,s3);
-					}
-				}
-				else if (cek)
-					s2 = s2+temp;
-				else if (temp.compareTo("\n")!=0)
-					s1 = s1+temp;
-				else
-				{
-					s1="";
-					s2="";
-					s3="";
-				}
-			}			
-			fin.close();
-		}
-		catch (Exception e)
-		{
-			System.err.println("Error: "+e.getMessage());
-		}
-	}
-	
-	public void writeoutput (String Bahasa, String namafile)
-	{
-		try
-		{
-			int bhs = (int)bahasa.get(Bahasa);
-			// Create file 
-			FileWriter fstream = new FileWriter(namafile);
-			BufferedWriter out = new BufferedWriter(fstream);
+/**
+ * Kelas model dari kamus ver 1
+ * @author Jordan, Adriel, and Wilson
+ */
+public class modelmapone{
+    
+    private SortedMap<String,Vector<twostringarray>> map[];
+    private int jumlah;
+    private Map<String,Integer> bahasa = new HashMap();
+    private String [] namabahasa;
+    private Map tipe = new HashMap(); // noun, verb, adj, other
 
-			Iterator iterator = map[bhs].keySet().iterator();
-			while (iterator.hasNext()) 
-			{
-				Object key = iterator.next();
-				out.write(key + "(" + tipe.get(key) + "):" + map[bhs].get(key) + ".\n");
-			}
-			
-			//Close the output stream
-			out.close();
-		}
-		catch (Exception e)
-		{	//Catch exception if any
-			System.err.println("Error: " + e.getMessage());
-		}
-	}
-	
+    /**
+     * constructor tanpa parameter untuk model map
+     */
+    public modelmapone ()
+    {
+           jumlah = 0;
+    }
+    
+    /**
+     * constructor dengan parameter untuk model map
+     * @param Jumlah Jumlah bahasa yang ada
+     * @param Bahasa Nama-nama bahasa
+     */
+    public modelmapone (int Jumlah, String [] Bahasa) 
+    {
+            jumlah=Jumlah;
+            map = new SortedMap[jumlah];
+            namabahasa = new String[Bahasa.length];
+            for (int i=0;i<jumlah;++i)
+            {
+                    map[i] = new TreeMap();
+            }
+            for (int i=0;i<Bahasa.length;++i)
+            {
+                    bahasa.put(Bahasa[i],i);
+                    namabahasa[i]=Bahasa[i];
+
+            }
+    }
+
+    /**
+     * prosedur untuk mencetak entri kamus dalam map
+     * @param Bahasa map bahasa yang dicetak
+     */
+    public void cetakmap (String Bahasa)
+    {
+        
+        if (bahasa.get(Bahasa)!=null)
+        {
+            int pos = bahasa.get(Bahasa);
+            viewmapone view = new viewmapone();
+            Iterator iterator = map[pos].keySet().iterator();
+            while (iterator.hasNext()) 
+            {
+                    String key = (String)iterator.next();
+                    view.cetakln(key + " : " + cetakvektor(map[pos].get(key)) + " (" + tipe.get(key) + ")");
+            }
+        }
+    }
+    
+    public String cetakvektor (Vector<twostringarray> v) {
+        String temp = v.get(0).getarti() + v.get(0).gettype();
+        int i = 1;
+        while (v.get(i)!=null) {
+            temp = temp + "," + v.get(i).getarti() + v.get(i).gettype();
+        }
+        return temp;
+    }
+    
+    /**
+     * fungsi untuk mendeteksi ada bahasa apa saja dalam map
+     * @param s Kata yang ingin dideteksi
+     * @return Bahasa-bahasa yang memiliki kata tersebut
+     */
+    public String [] detectmap (String s)
+    {
+            String [] value = new String[jumlah];
+            int j=0;
+            for (int i=0;i<jumlah;++i)
+            {
+                    if (map[i].get(s)!=null)
+                    {
+                            value[j]=namabahasa[i];
+                            j++;
+                    }
+            }
+            if (j>0)
+                return value;
+            return null;
+    }
+
+    /**
+     * getter value untuk map tipe
+     * @param s kata yang ingin diketahui tipenya
+     * @return tipe dari s
+     */
+    public String gettipe (String s)
+    {
+            if ((String) tipe.get(s)!=null)
+                    return (String) tipe.get(s);
+            return "";
+    }
+
+        /**
+     * fungsi untuk memperoleh terjemahan bahasa dari bahasa yg diinginkan
+     * @param Bahasa bahasa yang ingin diterjemahkan
+     * @param s kata yang ingin diterjemahkan
+     * @return terjemahan dari bahasa yang diingingkan
+     */
+    public Vector<twostringarray> getterjemahan (String Bahasa, String s)
+    {
+        if (bahasa.get(Bahasa)!=null)
+        {
+            int posisi = bahasa.get(Bahasa);
+            if (map[posisi].get(s)!=null){
+                    
+                    return map[posisi].get(s);
+            }
+        }
+        Vector<twostringarray> Vreturn = new Vector<twostringarray>();
+        return Vreturn;
+    }
+    
+    /**
+     * fungsi untuk mengisi entri kata dan terjemahan baru, mengembalikan 0 jika kata belum ada, mengembalikan -1 jika entri kata sudah ada
+     * @param Bahasa1 bahasa pertama
+     * @param Bahasa2 bahasa kedua
+     * @param s1 kata pertama
+     * @param s2 kata kedua
+     * @param s3 tipe kata
+     * @return nilai yang menandakan apakah kata berhasil dimasukkan
+     */
+    public int tambahterjemahan (String Bahasa1, String Bahasa2, String s1, String s2, String s3)
+    {
+        if ((bahasa.get(Bahasa1)!=null)&&(bahasa.get(Bahasa2)!=null))
+        {
+            int posisi1 = bahasa.get(Bahasa1);
+            int posisi2 = bahasa.get(Bahasa2);
+
+            boolean cek = false;
+//            if (map[posisi1].get(s1)==null)
+//            {
+                Vector<twostringarray> tempV = new Vector<twostringarray>();
+                tempV = map[posisi1].get(s1);
+                twostringarray stemp = new twostringarray(s2,s3);
+                tempV.add(stemp);
+                map[posisi1].put(s1,tempV);
+                tipe.put(s1,s3);
+//                cek = true;
+//            }
+//            if (map[posisi2].get(s2)==null)
+//            {
+                tempV = map[posisi1].get(s2);
+                stemp.setarti(s1);
+                tempV.add(stemp);
+                map[posisi2].put(s2,tempV);
+                tipe.put(s2,s3);
+                cek = true;
+ //           }
+            if (cek)
+                    return 0;
+        }
+        return -1;
+    }
+
+    /**
+     * getter untuk entri ke-i pada map Use modelmapone(int,String[]) to move a piece.
+     * @param i id map yang diinginkan
+     * @return map yang diinginkan
+     */
+    public SortedMap getmap (int i)
+    {
+            if (i<jumlah)
+                return map[i];
+            return null;
+    }
+
+    /**
+     * setter untuk map m pada indeks i
+     * @param m map yang ingin diset
+     * @param i id map yang ingin diset
+     */
+    public void setmap (SortedMap<String,Vector<twostringarray>> m, int i)
+    {
+        if (m!=null)
+        {
+            Iterator iterator = m.keySet().iterator();
+            while (iterator.hasNext()) 
+            {
+                    String key = (String)iterator.next();
+                    if (map[i].get(key)==null)
+                            map[i].put(key,m.get(key));
+            }
+        }
+    }
+
+    /**
+     * prosedur untuk membaca file dari luar
+     * @param bahasa1 bahasa pertama
+     * @param bahasa2 bahasa kedua
+     * @param namafile nama file.txt yang mengandung kamus dari bahasa1 ke bahasa2
+     */
+    public void readinput (String bahasa1, String bahasa2, String namafile)
+    {
+            try
+            {
+                    int bhs1 = bahasa.get(bahasa1);
+                    int bhs2 = bahasa.get(bahasa2);
+
+                    FileInputStream fstream = new FileInputStream(namafile);
+                    DataInputStream fin = new DataInputStream(fstream);
+                    BufferedReader brf = new BufferedReader(new InputStreamReader(fin));
+
+                    String s1="";
+                    String s2="";
+                    String s3="";
+                    Vector<twostringarray> tempV = new Vector<twostringarray>();
+					Vector<twostringarray> tempV1 = new Vector<twostringarray>();
+                    twostringarray stemp = new twostringarray();
+                    boolean cek = false;
+                    boolean cekspecial = false;
+                    int tempint;
+                    while ((tempint=brf.read())!=-1)
+                    {
+                            String temp = ""+(char)tempint;
+                            if (temp.compareTo(")")==0)
+                                    cekspecial = false;
+                            else if (cekspecial)
+                                    s3 = s3+temp;
+                            else if (temp.compareTo("(")==0)
+                                    cekspecial = true;
+                            else if (temp.compareTo(":")==0)
+                                    cek = true;
+                            else if (temp.compareTo(".")==0)
+                            {
+//									tempV = (Vector<twostringarray>) tempV.clone();
+//									System.out.println(tempV);
+									// tempV.removeAllElements();
+									// tempV1.removeAllElements();
+                                    cek = false;
+                                    if (map[bhs1].get(s1)!=null)
+                                    {
+                                        tempV = map[bhs1].get(s1);
+                                    }
+										stemp.setarti(s2);
+										stemp.settype(s3);										
+                                        tempV.add(stemp);
+                                        map[bhs1].put(s1,tempV);
+										tempV = new Vector<twostringarray>();
+                                        tipe.put(s1,s3);
+//										System.out.println(map[bhs1].get(s1).get(0).getarti() + " " + map[bhs1].get(s1).get(0).gettype());
+//                                    }
+//                                    tempV1 = (Vector<twostringarray>) tempV1.clone();
+                                    if (map[bhs2].get(s2)!=null)
+                                    {
+                                        tempV1 = map[bhs2].get(s2);
+                                    }
+                                        stemp.setarti(s1);
+                                        tempV1.add(stemp);
+                                        map[bhs2].put(s2,tempV1);
+										tempV1 = new Vector<twostringarray>();
+                                        tipe.put(s2,s3);
+//                                    }
+									if (map[bhs1].get("apel")!=null)
+										System.out.println(map[bhs1].get("apel").get(0).getarti());
+                            }
+                            else if (cek)
+                                    s2 = s2+temp;
+                            else if (temp.compareTo("\n")!=0)
+                                    s1 = s1+temp;
+                            else
+                            {
+                                    s1="";
+                                    s2="";
+                                    s3="";
+                            }
+                    }			
+                    fin.close();
+            }
+            catch (Exception e)
+            {
+                    System.err.println("Error readinput: "+e.getMessage());
+            }
+    }
+
+    /**
+     * prosedur untuk menuliskan isi map sekarang ke file luar
+     * @param Bahasa bahasa yang ingin ditulis
+     * @param namafile nama file
+     */
+    public void writeoutput (String Bahasa, String namafile)
+    {
+            try
+            {
+                    int bhs = bahasa.get(Bahasa);
+                    // Create file 
+                    FileWriter fstream = new FileWriter(namafile);
+                    BufferedWriter out = new BufferedWriter(fstream);
+
+                    Iterator iterator = map[bhs].keySet().iterator();
+                    while (iterator.hasNext()) 
+                    {
+                            String key = (String)iterator.next();
+                            out.write(key + "(" + tipe.get(key) + "):" + cetakvektor(map[bhs].get(key)) + ".\n");
+                    }
+
+                    //Close the output stream
+                    out.close();
+            }
+            catch (Exception e)
+            {	//Catch exception if any
+                    System.err.println("Error writeoutput: " + e.getMessage());
+            }
+    }
 }
